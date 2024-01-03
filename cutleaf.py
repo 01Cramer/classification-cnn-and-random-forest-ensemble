1e2a218961fc30a374dadbba5266c7f37d59cc96
01Cramer <adrian.sliwinski@student.put.poznan.pl>
2024-01-03 15:25:53 +0100

    before optimization

---
+++ cutleaf.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+
+img_path = "C:\\Users\\adria\\Documents\\GitHub\\leaf-count-cv\\data\\0005.jpg"
+output_dir = "C:\\Users\\adria\\Documents\\GitHub\\leaf-count-cv\\leafs"
+leaf_number = 50
+
+def cutleaf(img_path, output_dir, leaf_number):
+    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    ret, binary = cv2.threshold(img_gray, 230, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if w*h < 1000 or w*h > 2073599:     # 1920x1080
+            continue
+
+        leaf = img[y:y+h, x:x+w]
+
+        output_path = os.path.join(output_dir, str(leaf_number)+".jpg")
+        cv2.imwrite(output_path, leaf)
+
+        leaf_number += 1
+
+cutleaf(img_path, output_dir, leaf_number)
--- detect.py
+++ detect.py
@@ -1,32 +1,49 @@
-import json
-from pathlib import Path
 from typing import Dict
-from matplotlib import pyplot as plt
-import click
+import tensorflow as tf
 import cv2
+import os
 import numpy as np
-from tqdm import tqdm
-default_cmap = plt.cm.coolwarm
-import imageio
+def prepare(leaf_path):
+    leaf_size = 50
+    leaf_img = cv2.imread(leaf_path, cv2.IMREAD_GRAYSCALE)
+    leaf_img = cv2.resize(leaf_img, (leaf_size, leaf_size))
+    leaf_img = leaf_img/255.0
+    leaf_img = np.array(leaf_img).reshape(-1, leaf_size, leaf_size, 1)
+    return leaf_img
 
-img_path = "C:\\Users\\adria\\Documents\\GitHub\\leaf-count-cv\\data\\0001.jpg"
 
 def detect(img_path: str) -> Dict[str, int]:
-    """Object detection function, according to the project description, to implement.
+    output_dir = "C:\\Users\\adria\\Documents\\GitHub\\leaf-count-cv\\leafs"
+    file_list = os.listdir(output_dir)
+    for file_name in file_list:
+        file_path = os.path.join(output_dir, file_name)
+        try:
+            if os.path.isfile(file_path):
+                os.unlink(file_path)
+        except Exception as e:
+            print(f"Can't delete file {file_path}: {e}")
 
-    Parameters
-    ----------
-    img_path : str
-        Path to processed image.
-
-    Returns
-    -------
-    Dict[str, int]
-        A dictionary with the number of each object.
-    """
+    leaf_number = 0
     img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    # TODO: Implement detection method.
+    ret, binary = cv2.threshold(img_gray, 230, 255, cv2.THRESH_BINARY)
+    contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        (x, y, w, h) = cv2.boundingRect(contour)
+        if w*h < 1000 or w*h > 2073599:     # 1920x1080
+            continue
+
+        leaf = img[y:y+h, x:x+w]
+
+        output_path = os.path.join(output_dir, str(leaf_number)+".jpg")
+        cv2.imwrite(output_path, leaf)
+
+        leaf_number += 1
+
+#------------------------------------------------- NEURAL NET MODEL ---------------------------------------------------#
+    model = tf.keras.models.load_model('CNN_leafs.model', compile=False)
 
     aspen = 0
     birch = 0
@@ -34,26 +51,26 @@ def detect(img_path: str) -> Dict[str, int]:
     maple = 0
     oak = 0
 
+    leafs_to_classify = os.listdir(output_dir)
+
+    for leaf in leafs_to_classify:
+        leaf_path = os.path.join(output_dir, leaf)
+        leaf_transformed = prepare(leaf_path)
+
+        predictions = model.predict(leaf_transformed)
+        classification = np.argmax(predictions)
+
+        if classification == 0:
+            aspen += 1
+        elif classification == 1:
+            birch += 1
+        elif classification == 2:
+            hazel += 1
+        elif classification == 3:
+            maple += 1
+        else:
+            oak += 1
+
     return {'aspen': aspen, 'birch': birch, 'hazel': hazel, 'maple': maple, 'oak': oak}
 
-
-img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
-
-ret, binary = cv2.threshold(img,230,255,cv2.THRESH_BINARY)
-contours, hierarchy = cv2.findContours(binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-
-number_of_leafs = 0
-for contour in contours:
-    (x, y, w, h) = cv2.boundingRect(contour)
-    if w*h < 100 or w*h > 2073599: # 1920x1080
-        continue
-    print(x, y, w, h)
-    number_of_leafs += 1
-    # draw the bounding boxes
-    cv2.rectangle(binary, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-
-print("Number of leafs: ", number_of_leafs)
-plt.subplot(1, 1, 1)
-plt.imshow(binary, cmap='gray')
-plt.show()
+print(detect("C:\\Users\\adria\\Documents\\GitHub\\leaf-count-cv\\test_data\\0005.jpg"))
